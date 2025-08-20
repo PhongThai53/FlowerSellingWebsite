b2b01e33683914fe44f20fbf6fe9ab9c8b8dff96
Phong Thái <phongtdhe180210@fpt.edu.vn>
2025-08-20 16:58:37 +0700

    Fix Database

---
+++ Backend/Models/Entities/CartItem.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlowerSellingWebsite.Models.Entities
+{
+    public class CartItem : BaseEntity
+    {
+        [Required]
+        [ForeignKey("Cart")]
+        public int CartId { get; set; }
+
+        [Required]
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal UnitPrice { get; set; }
+
+        public decimal LineTotal { get; set; }
+
+        // Navigation properties
+        public virtual Cart? Cart { get; set; }
+        public virtual Products? Product { get; set; }
+    }
+}
---
+++ Backend/Models/Entities/Carts.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlowerSellingWebsite.Models.Entities
+{
+    public class Cart : BaseEntity
+    {
+        [Required]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        // Có thể gắn tạm cart với 1 order sau khi checkout
+        [ForeignKey("Order")]
+        public int? OrderId { get; set; }
+
+        public bool IsCheckedOut { get; set; } = false;
+
+        // Navigation properties
+        public virtual Users User { get; set; }
+        public virtual Orders? Order { get; set; }
+        public virtual ICollection<CartItem> CartItems { get; set; } = new HashSet<CartItem>();
+    }
+}
